d7e5ba0558bb48aaaca69e25639af5b23aa3ac64
brentgroves <brent.groves@gmail.com>
2024-11-01 12:25:53 -0400

    vin1: using constructors

---
+++ vin-stages/3/vin.go
@@ -0,0 +1,29 @@
+package vin
+
+import "fmt"
+
+type VIN string
+
+// it is debatable if this func should be named New or NewVIN
+// but NewVIN is better for greping and leaves room for other
+// NewXY funcs in the same package
+func NewVIN(code string)(VIN, error) {
+
+  if len(code) != 17 {
+    return "", fmt.Errorf("invalid VIN %s: more or less than 17 characters", code)
+  }
+
+  // ... check for disallowed characters ...
+
+  return VIN(code), nil
+}
+
+func (v VIN) Manufacturer() string {
+
+  manufacturer := v[: 3]
+  if manufacturer[2] == '9' {
+    manufacturer += v[11: 14]
+  }
+
+  return string(manufacturer)
+}
